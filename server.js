b188489f43a8d66b077301c8805f8d251afca375
Hélder Pereira <blackspiider.86@gmail.com>
2017-10-24 00:15:45 +0100

    added cors rules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,6 +9,16 @@ const indexRoutes = require('./routes/api');
 
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost:27017/interclub-challenge', { useMongoClient: true });
+
+app.use(cors());
+
+// accept request from everywhere
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 app.use('/api', indexRoutes);
 
